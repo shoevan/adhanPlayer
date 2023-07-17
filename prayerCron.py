db24e8e7c053288f10d7fa7f27c9183fb81b25f1
Sajib Ahmed <sajib.ahmed95@gmail.com>
2023-07-17 18:40:09 +1000

    update url request

--- prayerCron.py
+++ prayerCron.py
@@ -5,12 +5,15 @@ import os
 import getopt
 from crontab import CronTab, CronSlices
 
+
 def setCron(chromecast_name):
     cron = CronTab(user="pi")
     cron.remove_all()
-    prayerTimeFetch = cron.new(command=f"python3 {os.getenv('HOME')}/adhanPlayer/prayerTimes.py >> {os.getenv('HOME')}/prayerCron.txt", comment="prayerTimeFetch")
+    prayerTimeFetch = cron.new(
+        command=f"python3 {os.getenv('HOME')}/adhanPlayer/prayerTimes.py >> {os.getenv('HOME')}/prayerCron.txt", comment="prayerTimeFetch")
     prayerTimeFetch.setall("0 0 * * *")
-    setPrayerTime = cron.new(command=f"python3 {os.getenv('HOME')}/adhanPlayer/prayerCron.py -c \"{chromecast_name}\" >> {os.getenv('HOME')}/prayerCron.txt", comment="setPrayerTime")
+    setPrayerTime = cron.new(
+        command=f"python3 {os.getenv('HOME')}/adhanPlayer/prayerCron.py -c \"{chromecast_name}\" >> {os.getenv('HOME')}/prayerCron.txt", comment="setPrayerTime")
     setPrayerTime.setall("5 0 * * *")
     prayerNames = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"]
     x = 0
@@ -20,16 +23,17 @@ def setCron(chromecast_name):
                 fajrSwitch = "-f"
             else:
                 fajrSwitch = ""
-            lines = lines.strip().replace("\"","")
+            lines = lines.strip().replace("\"", "")
             print("Scheduling adhan for", lines)
 
-
-            timeSlot = lines[3:5] + " " + lines[0:2] + " * * *" 
+            timeSlot = lines[3:5] + " " + lines[0:2] + " * * *"
 #            print("Time slot valid: ", timeSlot, CronSlices.is_valid(timeSlot))
-            prayer = cron.new(f"python3 {os.getenv('HOME')}/adhanPlayer/castAdhan.py {fajrSwitch} -c \"{chromecast_name}\" >> {os.getenv('HOME')}/prayerCron.txt", comment=prayerNames[x])
+            prayer = cron.new(
+                f"python3 {os.getenv('HOME')}/adhanPlayer/castAdhan.py {fajrSwitch} -c \"{chromecast_name}\" >> {os.getenv('HOME')}/prayerCron.txt", comment=prayerNames[x])
             prayer.setall(timeSlot)
             x += 1
     cron.write()
+
 
 def main(argv):
     try:
@@ -41,6 +45,7 @@ def main(argv):
         if opt == "-c":
             chromecast_name = arg
     setCron(chromecast_name=chromecast_name)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- prayerTimes.py
+++ prayerTimes.py
@@ -1,12 +1,16 @@
-import json
-import urllib.request
 import os
+from requests import get
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+    "accept": "application/json"
+}
+
 
 def urlRequest(url):
-    req = urllib.request.Request(url)
-    response = urllib.request.urlopen(req)
-    data = response.read()
-    return json.loads(data)
+    resp = get(url=url, headers=headers)
+    return resp.json()
+
 
 def main():
     publicIP = urlRequest("https://api.ipify.org?format=json")["ip"]
@@ -14,8 +18,9 @@ def main():
     lat = location["lat"]
     lon = location["lon"]
     prayerTimeCalcMethod = 3
-    prayerTimeSchool =1
-    prayerTimes = urlRequest(f"http://api.aladhan.com/v1/timings/:date_or_timestamp?latitude={lat}&longitude={lon}&method={prayerTimeCalcMethod}&school={prayerTimeSchool}")["data"]["timings"]
+    prayerTimeSchool = 1
+    prayerTimes = urlRequest(
+        f"http://api.aladhan.com/v1/timings/:date_or_timestamp?latitude={lat}&longitude={lon}&method={prayerTimeCalcMethod}&school={prayerTimeSchool}")["data"]["timings"]
     print(prayerTimes)
     with open(f"{os.environ['HOME']}/prayerTimes.txt", "w") as prayers:
         prayers.write(f"{prayerTimes['Fajr']}\n")
@@ -23,6 +28,7 @@ def main():
         prayers.write(f"{prayerTimes['Asr']}\n")
         prayers.write(f"{prayerTimes['Maghrib']}\n")
         prayers.write(f"{prayerTimes['Isha']}\n")
+
 
 if __name__ == "__main__":
     main()
